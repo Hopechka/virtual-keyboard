90cc9507b6f23837b223576ebdc10eae0cf7c126
Nadezhda Budarina <hopechka@gmail.com>
2022-05-01 13:43:52 +0300

    feat:add keyboard printing and display in textarea
    1.  change classes for double button
    2. press effect for buttons
    3. font for textarea

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ function btnsCreater(size) {
     BTN.classList.add('keyboard_key-extra-long');
   }
   if (BTN.innerHTML === 'caps lock') {
-    BTN.classList.add('caps-lock');
+    BTN.classList.add('capslock');
   }
   if (BTN.innerHTML === 'shift') {
     BTN.classList.add('shift');
@@ -44,33 +44,59 @@ function btnsCreater(size) {
     BTN.innerHTML ===
     '<span class="material-icons left">keyboard_command_key</span>'
   ) {
-    BTN.classList.add('command-left');
+    BTN.classList.add('metaleft');
   }
   if (
     BTN.innerHTML ===
     '<span class="material-icons right">keyboard_command_key</span>'
   ) {
-    BTN.classList.add('command-right');
+    BTN.classList.add('metaright');
   }
   if (
     BTN.innerHTML === '<span class="material-icons">keyboard_arrow_right</span>'
   ) {
-    BTN.classList.add('right');
+    BTN.classList.add('arrowright');
   }
   if (
     BTN.innerHTML === '<span class="material-icons">keyboard_arrow_left</span>'
   ) {
-    BTN.classList.add('left');
+    BTN.classList.add('arrowleft');
   }
   if (
     BTN.innerHTML === '<span class="material-icons">keyboard_arrow_up</span>'
   ) {
-    BTN.classList.add('up');
+    BTN.classList.add('arrowup');
   }
   if (
     BTN.innerHTML === '<span class="material-icons">keyboard_arrow_down</span>'
   ) {
-    BTN.classList.add('down');
+    BTN.classList.add('arrowdown');
+  }
+  if (BTN.innerHTML === '<span class="material-icons">keyboard_tab</span>') {
+    BTN.classList.add('tab');
+  }
+  if (
+    BTN.innerHTML === '<span class="material-icons">keyboard_backspace</span>'
+  ) {
+    BTN.classList.add('backspace');
+  }
+  if (BTN.innerHTML === '<span class="shift-left">shift</span>') {
+    BTN.classList.add('shiftleft');
+  }
+  if (BTN.innerHTML === '<span class="shift-right">shift</span>') {
+    BTN.classList.add('shiftright');
+  }
+  if (BTN.innerHTML === 'control') {
+    BTN.classList.add('controlleft');
+  }
+  if (BTN.innerHTML === '<span class="alt-left">option</span>') {
+    BTN.classList.add('altleft');
+  }
+  if (BTN.innerHTML === '<span class="alt-right">option</span>') {
+    BTN.classList.add('altright');
+  }
+  if (BTN.innerHTML === 'enter') {
+    BTN.classList.add('enter');
   }
   letter += 1;
   return BTN;
@@ -112,11 +138,12 @@ function creatLayout() {
     'Клавиатура создана в операционной системе MAC OS <br/> Для переключения языка комбинация: command+space';
 
   // добавляю функционал для caps lock
-  const CAPS_LOCK = document.querySelector('.caps-lock');
-  const COMMAND_LEFT = document.querySelector('.command-left');
-  const COMMAND_RIGHT = document.querySelector('.command-right');
+  const CAPS_LOCK = document.querySelector('.capslock');
+  const COMMAND_LEFT = document.querySelector('.metaleft');
+  const COMMAND_RIGHT = document.querySelector('.metaright');
   const keyboardKeyArray = document.querySelectorAll('.keyboard_key');
-  const SHIFT_LIST = document.querySelectorAll('.shift');
+  const SHIFT_LEFT = document.querySelector('.shiftleft');
+  const SHIFT_RIGHT = document.querySelector('.shiftright');
 
   function registerChanger() {
     CAPS_LOCK.classList.toggle('active');
@@ -188,16 +215,17 @@ function creatLayout() {
     }
   }
   function anShiftSwitching() {
+    if (lang === 'en') {
+      registerChoose = ENG_LOWER_CASE;
+    } else {
+      registerChoose = RUS_LOWER_CASE;
+    }
     if (CAPS_LOCK.classList.contains('active')) {
       if (lang === 'en') {
         registerChoose = ENG_UPPER_CASE;
       } else {
         registerChoose = RUS_UPPER_CASE;
       }
-    } else if (lang === 'en') {
-      registerChoose = ENG_LOWER_CASE;
-    } else {
-      registerChoose = RUS_LOWER_CASE;
     }
 
     for (let i = 0; i < 65; i += 1) {
@@ -215,10 +243,125 @@ function creatLayout() {
   CAPS_LOCK.addEventListener('click', registerChanger);
   COMMAND_LEFT.addEventListener('click', languageSwitching);
   COMMAND_RIGHT.addEventListener('click', languageSwitching);
-  SHIFT_LIST[0].addEventListener('mousedown', shiftSwitching);
-  SHIFT_LIST[1].addEventListener('mousedown', shiftSwitching);
-  SHIFT_LIST[0].addEventListener('mouseup', anShiftSwitching);
-  SHIFT_LIST[1].addEventListener('mouseup', anShiftSwitching);
+  SHIFT_LEFT.addEventListener('mousedown', shiftSwitching);
+  SHIFT_RIGHT.addEventListener('mousedown', shiftSwitching);
+  SHIFT_LEFT.addEventListener('mouseup', anShiftSwitching);
+  SHIFT_RIGHT.addEventListener('mouseup', anShiftSwitching);
+
+  // эмуляция нажатия клавиш на вирт. клавиатуре
+  function virtualKeyboardPress(key, code) {
+    for (let i = 0; i < 65; i += 1) {
+      if (keyboardKeyArray[i].innerHTML === key) {
+        keyboardKeyArray[i].classList.add('press');
+      }
+    }
+    for (let i = 0; i < 65; i += 1) {
+      if ((i >= 13 && i <= 14) || i === 28 || (i >= 40 && i <= 41) || i >= 52) {
+        if (keyboardKeyArray[i].classList.contains(code.toLowerCase())) {
+          keyboardKeyArray[i].classList.add('press');
+          if (i === 28) {
+            registerChanger();
+          }
+          if (i === 41 || i === 54) {
+            shiftSwitching();
+          }
+        }
+      }
+    }
+  }
+  function virtualKeyboardUnPress(key, code) {
+    for (let i = 0; i < 65; i += 1) {
+      if (keyboardKeyArray[i].innerHTML === key) {
+        keyboardKeyArray[i].classList.remove('press');
+      }
+    }
+    for (let i = 0; i < 65; i += 1) {
+      if ((i >= 13 && i <= 14) || i === 28 || (i >= 40 && i <= 41) || i >= 52) {
+        if (keyboardKeyArray[i].classList.contains(code.toLowerCase())) {
+          keyboardKeyArray[i].classList.remove('press');
+          if (i === 28) {
+            registerChanger();
+          }
+          if (i === 41 || i === 54) {
+            anShiftSwitching();
+          }
+        }
+      }
+    }
+  }
+
+  // переназначение клавиш клавиатуры
+  const TEXTAREA_SCREEN = document.querySelector('.screen');
+
+  document.addEventListener('keydown', (e) => {
+    //console.log(e);
+    virtualKeyboardPress(e.key, e.code);
+    try {
+      e.preventDefault();
+      if (e.key === 'Tab') {
+        TEXTAREA_SCREEN.value += '   ';
+      } else if (e.key === 'Enter') {
+        TEXTAREA_SCREEN.value += '\n';
+      } else if (
+        e.key === 'Alt' ||
+        e.key === 'Control' ||
+        e.key === 'CapsLock' ||
+        e.key === 'Shift' ||
+        e.key === 'Meta'
+      ) {
+        TEXTAREA_SCREEN.value += '';
+      } else if (e.key === 'ArrowUp') {
+        TEXTAREA_SCREEN.value += '▲';
+      } else if (e.key === 'ArrowDown') {
+        TEXTAREA_SCREEN.value += '▼';
+      } else if (e.key === 'ArrowLeft') {
+        TEXTAREA_SCREEN.value += '◀';
+      } else if (e.key === 'ArrowRight') {
+        TEXTAREA_SCREEN.value += '▶';
+      } else if (e.key === 'Backspace') {
+        const str = TEXTAREA_SCREEN.value;
+        TEXTAREA_SCREEN.value = str.substr(0, str.length - 1);
+      } else {
+        TEXTAREA_SCREEN.value += e.key;
+      }
+    } catch (i) {
+      i;
+    }
+    // if (e.key === 'Tab') {
+    //   TEXTAREA_SCREEN.value += '   ';
+    // } else if (e.key === 'Enter') {
+    //   TEXTAREA_SCREEN.value += '\n';
+    // } else if (
+    //   e.key === 'Alt' ||
+    //   e.key === 'Control' ||
+    //   e.key === 'CapsLock' ||
+    //   e.key === 'Shift' ||
+    //   e.key === 'Meta' ||
+    //   e.key === 'Backspace'
+    // ) {
+    //   TEXTAREA_SCREEN.value += '';
+    // } else if (e.key === 'ArrowUp') {
+    //   TEXTAREA_SCREEN.value += '▲';
+    // } else if (e.key === 'ArrowDown') {
+    //   TEXTAREA_SCREEN.value += '▼';
+    // } else if (e.key === 'ArrowLeft') {
+    //   TEXTAREA_SCREEN.value += '◀';
+    // } else if (e.key === 'ArrowRight') {
+    //   TEXTAREA_SCREEN.value += '▶';
+    // } else {
+    //   TEXTAREA_SCREEN.value += e.key;
+    // }
+  });
+
+  document.addEventListener('keyup', (e) => {
+    virtualKeyboardUnPress(e.key, e.code);
+  });
+
+  document.addEventListener('click', (e) => {
+    const target = e.target.closest('.keyboard_key');
+    console.log(target.innerHTML);
+    TEXTAREA_SCREEN.value += target.innerHTML;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', creatLayout);
